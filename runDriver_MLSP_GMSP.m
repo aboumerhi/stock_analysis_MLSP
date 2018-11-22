938522676c924a2826f3279327941d14b9c282ca
Khaled Aboumerhi <44167834+kaboume1@users.noreply.github.com>
2018-11-22 18:10:07 -0500

    Add files via upload

--- runDriver_MLSP_GMSP.m
+++ runDriver_MLSP_GMSP.m
@@ -1,104 +1,120 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% COURSE:              Machine Learning for Signal Processing 
-% AUTHORS:             Ashley Kiemen, Khaled Aboumerhi, & Huixiang Li 
+% COURSE:              Machine Learning for Signal Processing
+% AUTHORS:             Khaled Aboumerhi, Ashley Kiemen & Huixiang Li
 % ASSIGNMENT:          Group Project
 % TITLE:               General Motors Stock Prediction
-% DUE DATE:            November 14, 2018 
-% LAST DATE: REVISED:  11/14/18
-% REVISION ASSIGNMENT: rev A
+% DUE DATE:            December XX, 2018
+% LAST DATE: REVISED:  11/22/18
+% REVISION ASSIGNMENT: rev B
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all; clc;
 
-%% INITIALIZE VARIABLES FOR IMPORTING DATA
+%% IMPORT DATA
 
 cd '../data/FinalData/over1000DataPts'
 
 list = dir;
 
-listSize = size(list, 1);
-listSize = listSize - 2;
+[listSize, ~] = size(list);
+numFiles = listSize - 2;
 
-fileNames = strings(listSize, 1);
-sizes = zeros(listSize, 2);
-openData = cell(listSize, 2);
-highData = cell(listSize, 2);
-lowData = cell(listSize, 2);
-closeData = cell(listSize, 2);
-volumeData = cell(listSize, 2);
-timeData = cell(listSize, 2);
-masterData = cell(listSize, 2);
+fileNames = strings(numFiles, 1);
+sizes = zeros(numFiles, 1);
+masterData = cell(numFiles, 1);
+masterDataScaled = cell(numFiles, 1);
 
-openData_f = cell(listSize, 2);
-highData_f = cell(listSize, 2);
-lowData_f = cell(listSize, 2);
-closeData_f = cell(listSize, 2);
-volumeData_f = cell(listSize, 2);
-timeData_f = cell(listSize, 2);
-masterData_f = cell(listSize, 2);
-
-%% IMPORT DATA
-
-for i = 3:size(list)
-        
+%this is because the first two files in the list are prior directories
+for i = 3:(numFiles + 2)
+    
     fileNames(i - 2) = string(list(i).name);
     info = importdata(fileNames(i - 2));
-    timeVec = info.textdata;
     allData = info.data;
     
     sizes(i - 2) = size(allData, 1);
-    
-    opens = allData(:, 1);
-    highs = allData(:, 2);
-    lows = allData(:, 3);
-    closes = allData(:, 4);
-    volume = allData(:, 5);
-    
-    T = timeVec([2:size(timeVec, 1)], 1);
-    
-    openData{i - 2, 1} = opens;
-    highData{i - 2, 1} = highs;
-    lowData{i - 2, 1} = lows;
-    closeData{i - 2, 1} = closes;
-    volumeData{i - 2, 1} = volume;
-    timeData{i - 2, 1} = T;    
-    masterData{i - 2, 1} = allData;
+    masterData{i - 2} = allData;
     
 end
 
-%% RESIZE DATA BASED ON SMALLEST SET
+%% RESIZE DATA BASED ON THE SMALLEST SET w/ & w/o DOW INFORMATION
 
 minSize = min(sizes);
 
-for i = 1:listSize
-    
-    A = openData{i};
-    B = highData{i};
-    C = lowData{i};
-    D = closeData{i};
-    E = volumeData{i};
-    F = timeData{i};
-    G = masterData{i};
+openDataDOW = zeros(minSize, numFiles);
+highDataDOW = zeros(minSize, numFiles);
+lowDataDOW = zeros(minSize, numFiles);
+closeDataDOW = zeros(minSize, numFiles);
+volumeDataDOW = zeros(minSize, numFiles);
 
-    openData_f{i, 1} = A([1:minSize], :);
-    highData_f{i, 1} = B([1:minSize], :);
-    lowData_f{i, 1} = C([1:minSize], :);
-    closeData_f{i, 1} = D([1:minSize], :);
-    volumeData_f{i, 1} = E([1:minSize], :);
-    timeData_f{i, 1} = F([1:minSize], :);
-    masterData_f{i, 1} = G([1:minSize], :);
+openData = zeros(minSize, numFiles - 1);
+highData = zeros(minSize, numFiles - 1);
+lowData = zeros(minSize, numFiles - 1);
+closeData = zeros(minSize, numFiles - 1);
+volumeData = zeros(minSize, numFiles - 1);
+
+masterSets = cell(5, 2);
+
+for i = 1:numFiles
+    
+    A = masterData{i};
+    A = A([1:minSize], :);
+    
+    masterDataScaled{i} = A;
+    
+    openDataDOW(:, i) = A(:, 1);
+    highDataDOW(:, i) = A(:, 2);
+    lowDataDOW(:, i) = A(:, 3);
+    closeDataDOW(:, i) = A(:, 4);
+    volumeDataDOW(:, i) = A(:, 5);
+    
+    masterSets{1, 1} = openDataDOW;
+    masterSets{2, 1} = highDataDOW;
+    masterSets{3, 1} = lowDataDOW;
+    masterSets{4, 1} = closeDataDOW;
+    masterSets{5, 1} = volumeDataDOW;
+    
+    if i ~= 1
+        
+        openData(:, i - 1) = A(:, 1);
+        highData(:, i - 1) = A(:, 2);
+        lowData(:, i - 1) = A(:, 3);
+        closeData(:, i - 1) = A(:, 4);
+        volumeData(:, i - 1) = A(:, 5);
+        
+        masterSets{1, 2} = openData;
+        masterSets{2, 2} = highData;
+        masterSets{3, 2} = lowData;
+        masterSets{4, 2} = closeData;
+        masterSets{5, 2} = volumeData;
+                
+    end
+    
 end
 
-clear A B C D E F G i;
-clear opens highs lows closes volumes T;
-clear info timeVec allData sizes;
+clear A i;
+clear info allData sizes;
 
 cd '../../../src'
 
+%% PERFORM PCA ANALYSIS ON THE MODELS
+
+corrMat = cell(5, 2);
+
+for i = 1:size(corrMat, 1)
+    
+    X1 = masterSets{i, 1};
+    X2 = masterSets{i, 2};
+    
+    corrMat{i, 1} = pca(X1);
+    corrMat{i, 2} = pca(X2); 
+    
+end
+
+clear X1 X2 i;
 
 
 
